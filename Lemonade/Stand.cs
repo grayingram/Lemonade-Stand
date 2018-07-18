877aa07a7798205073025dd8d8f8023612a1e509
Bill Grayson Carol Ingram Grayson Sons Ingram <grayson.ingram@att.net>
2018-07-18 09:53:11 -0500

    Created a Project Lemonade with a class Stand. Class stand has public fields, for now, including name, predictive sell, price, and owner deductible

---
+++ Lemonade/Stand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lemonade
+{
+    class Stand
+    {
+        public string Name { get; set; }
+        public int Predictivesell { get; set; }
+        public decimal Price { get; set; }
+        public decimal MyDededuct { get; set; }
+
+    }
+}
