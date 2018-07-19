b506a300a713132db67ccd7b1fb40ff51c6f1e9b
Bill Grayson Carol Ingram Grayson Sons Ingram <grayson.ingram@att.net>
2018-07-19 09:32:49 -0500

    Added a lawyer class to use the methods created. Also fixed a bug where making sure a decimal was not negative was calling the wrong get method.

---
+++ Lemonade/Lawyer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lemonade
+{
+    class Lawyer
+    {
+        public string getResponse(string question)
+        {
+            Console.WriteLine(question);
+            string response = Console.ReadLine();
+            while (String.IsNullOrWhiteSpace(response))
+            {
+                Console.WriteLine("Please enter an actual value for the question\n" + question);
+                response = Console.ReadLine();
+            }
+
+            return response;
+        }
+        public int getInt(string question)
+        {
+            string response = getResponse(question);
+            int userInput;
+
+            while (int.TryParse(response, out userInput) == false)
+            {
+                Console.WriteLine("Unable to determine number. ");
+                response = getResponse(question);
+            }
+            userInput = notNeg(userInput, response, question);
+
+            return userInput;
+        }
+        public int notNeg(int num, string answer, string query)
+        {
+
+            int userInput = 0;
+
+            while ((num < 0) || (num == 0))
+            {
+                Console.WriteLine("Must be a nonnegative nonzero number");
+                num = getInt(query);
+            }
+            userInput = num;
+            return userInput;
+        }
+        public decimal notNeg(decimal num, string answer, string query)
+        {
+            decimal userInput = 0;
+
+            while ((num < 0) || (num == 0))
+            {
+                Console.WriteLine("Must be a nonnegative nonzero number");
+                num = getDecimal(query);
+            }
+            userInput = num;
+            return userInput;
+        }
+        public decimal getDecimal(string question)
+        {
+            string response = getResponse(question);
+            decimal userInput = 0;
+
+            while (decimal.TryParse(response, out userInput) == false)
+            {
+                Console.WriteLine("Unable to determine number. ");
+                response = getResponse(question);
+            }
+            userInput = notNeg(userInput, response, question);
+
+            return userInput;
+        }
+    }
+}
--- Lemonade/Program.cs
+++ Lemonade/Program.cs
@@ -7,81 +7,15 @@ namespace Lemonade
         static void Main(string[] args)
         {
             Stand ownerStand = new Stand();
-            ownerStand.Name = getResponse("What do you want to name the lemonade stand?");
-            ownerStand.Predictivesell = getInt("How many cups do you plan to sell?");
-            ownerStand.Price = getDecimal("How much do you want to charge for cup of lemonade?");
-            ownerStand.OwnerLoss = getDecimal("How much will this cost you per cup?");
-
-            
+            Lawyer ownerLawyer = new Lawyer();
+            ownerStand.Name = ownerLawyer.getResponse("What do you want to name the lemonade stand?");
+            ownerStand.Predictivesell = ownerLawyer.getInt("How many cups do you plan to sell?");
+            ownerStand.Price = ownerLawyer.getDecimal("How much do you want to charge for cup of lemonade?");
+            ownerStand.OwnerLoss = ownerLawyer.getDecimal("How much will this cost you per cup?");
             Console.WriteLine(ownerStand.RevealResult());
             Console.ReadLine();
             
-        }
-
-        static string getResponse(string question)
-        {
-            Console.WriteLine(question);
-            string response = Console.ReadLine();
-            while (String.IsNullOrWhiteSpace(response))
-            {
-                Console.WriteLine("Please enter an actual value for the question\n" + question);
-                response = Console.ReadLine();
-            }
-            return response;
-        }
-        static int getInt(string question)
-        {
-            string response = getResponse(question);
-            int userInput;
-
-            while (int.TryParse(response, out userInput) == false)
-            {
-               Console.WriteLine("Unable to determine number. ");
-               response = getResponse(question);
-            }
-            userInput = notNeg(userInput, response, question);    
-            
-            return userInput;
-        }
-        static int notNeg(int num, string answer, string query)
-        {
-            
-            int userInput = 0;
-
-            while ((num < 0) || (num == 0))
-            {
-                Console.WriteLine("Must be a nonnegative nonzero number");
-                num = getInt(query);
-            }
-            userInput = num;
-            return userInput;
-        }
-        static decimal notNeg(decimal num,  string answer, string query) 
-        {
-            decimal userInput = 0;
-
-            while ((num < 0)||(num == 0))
-            {
-                Console.WriteLine("Must be a nonnegative nonzero number");
-                num = getInt(query);
-            }
-            userInput = num;
-            return userInput;
-        }
-        static decimal getDecimal(string question)
-        {
-            string response = getResponse(question);
-            decimal userInput = 0;
-           
-            while (decimal.TryParse(response, out userInput) == false)
-            {
-                    Console.WriteLine("Unable to determine number. ");
-                    response = getResponse(question);
-            }
-            userInput = notNeg(userInput, response, question);
-            
-            return userInput;
-        }
+        }       
 
     }
 }
