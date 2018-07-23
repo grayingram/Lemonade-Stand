ad964a02c851750a3baf033d53d449ce20d9c543
Bill Grayson Carol Ingram Grayson Sons Ingram <grayson.ingram@att.net>
2018-07-23 10:11:28 -0500

    Minor change

--- Lemonade/LemonadeCorporation.cs
+++ Lemonade/LemonadeCorporation.cs
@@ -31,7 +31,7 @@ namespace Lemonade
             {
                 Console.WriteLine("Here are the results for stand #" + count);
                 Console.WriteLine("The name of the stand is planned to be: " + mystand.Name);
-                Console.WriteLine("The revenue is predicted for the stand is to be: " + mystand.Profit);
+                Console.WriteLine("The revenue is predicted for the stand is to be: " + mystand.Revenue);
                 Console.WriteLine("The expenses is predicted for the stand is to be: " + mystand.Expenses);
                 Console.WriteLine("The profit is predicted for the stand is to be: " + mystand.Profit);
                 count++;
