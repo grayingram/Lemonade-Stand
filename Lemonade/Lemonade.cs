2ed288aef168a895fc9d39cd1c9082d42689e34e
Bill Grayson Carol Ingram Grayson Sons Ingram <grayson.ingram@att.net>
2018-07-24 10:54:36 -0500

    Fixed the output to print correctly

--- Lemonade/Lemonade.cs
+++ Lemonade/Lemonade.cs
@@ -6,19 +6,21 @@ namespace LemonadeStands
 {
     class Lemonade : Stand
     {
+        public string type = "lemonade";
+        public string item = "cup";
         public List<Lemonade> SetLemonStands(Lawyer myLawyer, List<Lemonade> aStand)
         {
-            int numofStands = myLawyer.GetInt("How many lemonade stands do you want to create?");
+            int numofStands = myLawyer.GetInt("How many " + type + " stands do you want to create?");
 
             for (int i = 0; i < numofStands; i++)
             {
                 var ownerStand = new Lemonade();
                 Console.WriteLine("For Stand #" + (i + 1));
-                ownerStand.Name = myLawyer.GetResponse("What do you want to name this lemonade stand?");
+                ownerStand.Name = myLawyer.GetResponse("What do you want to name this " + type + " stand?");
 
-                ownerStand.Predictivesell = myLawyer.GetInt("How many cups do you plan to sell at this stand?");
-                ownerStand.Price = myLawyer.GetDecimal("How much do you want to charge for cup of lemonade at this stand?");
-                ownerStand.OwnerLoss = myLawyer.GetDecimal("How much will this cost you per cup for this stand?");
+                ownerStand.Predictivesell = myLawyer.GetInt("How many " + item + "s do you plan to sell at this stand?");
+                ownerStand.Price = myLawyer.GetDecimal("How much do you want to charge for a " + item + " of " + type + " at this stand?");
+                ownerStand.OwnerLoss = myLawyer.GetDecimal("How much will this cost you per " + item + " for this stand?");
                 aStand.Add(ownerStand);
             }
             return aStand;
--- Lemonade/Popsicle.cs
+++ Lemonade/Popsicle.cs
@@ -6,19 +6,21 @@ namespace LemonadeStands
 {
     class Popsicle : Stand
     {
+        public string type = "popsicle";
+        public string item = "stick";
         public List<Popsicle> SetPopsicleStands(Lawyer myLawyer, List<Popsicle> aStand)
         {
-            int numofStands = myLawyer.GetInt("How many lemonade stands do you want to create?");
+            int numofStands = myLawyer.GetInt("How many " + type + " stands do you want to create?");
 
             for (int i = 0; i < numofStands; i++)
             {
                 var ownerStand = new Popsicle();
                 Console.WriteLine("For Stand #" + (i + 1));
-                ownerStand.Name = myLawyer.GetResponse("What do you want to name this lemonade stand?");
+                ownerStand.Name = myLawyer.GetResponse("What do you want to name this " + type + " stand?");
 
-                ownerStand.Predictivesell = myLawyer.GetInt("How many cups do you plan to sell at this stand?");
-                ownerStand.Price = myLawyer.GetDecimal("How much do you want to charge for cup of lemonade at this stand?");
-                ownerStand.OwnerLoss = myLawyer.GetDecimal("How much will this cost you per cup for this stand?");
+                ownerStand.Predictivesell = myLawyer.GetInt("How many "+ item + "s do you plan to sell at this stand?");
+                ownerStand.Price = myLawyer.GetDecimal("How much do you want to charge for a " + item + " of " + type + " at this stand?");
+                ownerStand.OwnerLoss = myLawyer.GetDecimal("How much will this cost you per " + item + " for this stand?");
                 aStand.Add(ownerStand);
             }
             return aStand;
